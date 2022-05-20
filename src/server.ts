e310f02a3dace0515ad44cc49db02f8cc3aa6a8b
Njoli Patrick <ogmaro@gmail.com>
2022-05-20 13:51:24 +0100

    added style enforcers(eslint and prettier)

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+import app from './app';
+import dotenv from 'dotenv';
+dotenv.config();
+const { PORT, ADDRESS } = process.env;
+
+app.listen(PORT, () => {
+	console.log(`Server running on ${ADDRESS} ${PORT}`);
+});
