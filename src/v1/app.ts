5908d329f7dc7837194ec1e9ff51afe577e208bd
Njoli Patrick <ogmaro@gmail.com>
2022-05-27 15:48:19 +0100

    remove all auth code with & error handlers

--- src/v1/app.ts
+++ src/v1/app.ts
@@ -1,24 +1,9 @@
 import express, { Request, Response, Application, NextFunction, urlencoded, json } from 'express';
-import router from './route/index.router';
-import CustomError from './utile/error.utile';
 const app: Application = express();
 
 app.use(json());
 app.use(urlencoded({ extended: true }));
 
-app.use('/api/v1', router);
 
-app.all('*', (req, res, next) => {
-	throw new CustomError(`Requested URL http://${req.hostname}${req.path} not found`, 404);
-});
-
-app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-	const statusCode = err.statusCode || 500;
-	res.status(statusCode).json({
-		success: false,
-		message: err.message,
-		stack: err.stack
-	});
-});
 
 export default app;
