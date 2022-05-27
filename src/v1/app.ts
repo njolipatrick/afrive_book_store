4bcfbbca3eec7c14e82efe96d8ffa3ec6c43fd76
Njoli Patrick <ogmaro@gmail.com>
2022-05-27 16:39:08 +0100

    errorv1

--- src/v1/app.ts
+++ src/v1/app.ts
@@ -4,6 +4,22 @@ const app: Application = express();
 app.use(json());
 app.use(urlencoded({ extended: true }));
 
+app.use('/test', (req: Request, res: Response) => {
+	res.status(200).json({ status: 'ok', success: true });
+});
 
+app.all('*', (req, res, next) => {
+	const err = new Error(`Route http//:${req.hostname}${req.path} not found `);
+	next(err);
+});
+
+app.use((err: { statusCode: number; message: string; stack: string; }, req: Request, res: Response, next: NextFunction) => {
+	const statusCode = err.statusCode || 500;
+	res.status(statusCode).json({
+		success: false,
+		message: err.message,
+		stack: err.stack
+	});
+});
 
 export default app;
