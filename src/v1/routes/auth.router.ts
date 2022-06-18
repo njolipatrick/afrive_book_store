ac83ec67593a769474fe9430d2dbcf820311cf0c
Njoli Patrick <ogmaro@gmail.com>
2022-06-18 17:55:30 +0100

    removed middleware

--- src/v1/routes/auth.router.ts
+++ src/v1/routes/auth.router.ts
@@ -6,12 +6,12 @@ const auth = Router();
 
 auth.post('/register', upload.single('avatar'), authController.register);
 auth.get('/google-login-url', authController.googleAuthURL);
-auth.post('/google-login', authController.googleAuthUser); 
+auth.post('/google-login', authController.googleAuthUser);
 auth.post('/login', authController.login);
 auth.get('/verify/:email/:token', authController.verifyEmail);
 auth.post('/send-reset-password-link', authController.SendResetPasswordMail);
 auth.post('/reset-password', authController.ResetPassword);
-auth.post('/test', verifyToken, (req, res) => {
+auth.post('/test', (req, res) => {
     res.status(200).json({ message: 'Ok' });
 });
 
