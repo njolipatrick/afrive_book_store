5a10560ebefbecbce0fe2e196b1f26f63df9bd7a
Njoli Patrick <ogmaro@gmail.com>
2022-06-22 07:51:41 +0100

    minor fix fix

--- src/v1/utiles/google.auth.ts
+++ src/v1/utiles/google.auth.ts
@@ -1,6 +1,6 @@
 const GOOGLE_CLIENT_ID = String(process.env.GOOGLE_CLIENT_ID);
 const GOOGLE_CLIENT_SECRET = String(process.env.GOOGLE_CLIENT_SECRET);
-const SERVER_ROOT_URI = 'https://afrive-book-store.herokuapp.com/api/v1/auth/google-login';
+const SERVER_ROOT_URI = 'https://afrive-books.vercel.app/api/v1/auth';
 const rootUrl = 'https://accounts.google.com/o/oauth2/v2/auth';
 import axios from 'axios';
 import CustomError from './error.utile';
