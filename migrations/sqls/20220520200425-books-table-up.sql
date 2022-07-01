df44205ca1fdd480d21da680adfdfcd577ffbce2
Njoli Patrick <ogmaro@gmail.com>
2022-07-01 13:13:25 +0100

    seeded books

--- migrations/sqls/20220520200425-books-table-up.sql
+++ migrations/sqls/20220520200425-books-table-up.sql
@@ -10,34 +10,10 @@ CREATE TABLE books (
     created_at timestamp default current_timestamp,
     updated_at timestamp default current_timestamp
 );
-INSERT INTO books(title, author, image, description, price, status )VALUES ('Tales By MoonLight', 'Njoli Patrick1', 'https://ibb.co/z4Q4L1N', 'this is a new book', 500.0, 'available');
-INSERT INTO books(title, author, image, description, price, status )VALUES ('Tales By MoonLight', 'Njoli Patrick2', 'https://ibb.co/z4Q4L1N', 'this is a new book', 500.0, 'available');
-INSERT INTO books(title, author, image, description, price, status )VALUES ('Tales By MoonLight', 'Njoli Patrick3', 'https://ibb.co/z4Q4L1N', 'this is a new book', 500.0, 'available');
-INSERT INTO books(title, author, image, description, price, status )VALUES ('Tales By MoonLight', 'Njoli Patrick4', 'https://ibb.co/z4Q4L1N', 'this is a new book', 500.0, 'available');
-INSERT INTO books(title, author, image, description, price, status )VALUES ('Tales By MoonLight', 'Njoli Patrick5', 'https://ibb.co/z4Q4L1N', 'this is a new book', 500.0, 'available');
-INSERT INTO books(title, author, image, description, price, status )VALUES ('Tales By MoonLight', 'Njoli Patrick6', 'https://ibb.co/z4Q4L1N', 'this is a new book', 500.0, 'available');
-INSERT INTO books(title, author, image, description, price, status )VALUES ('Tales By MoonLight', 'Njoli Patrick7', 'https://ibb.co/z4Q4L1N', 'this is a new book', 500.0, 'available');
-INSERT INTO books(title, author, image, description, price, status )VALUES ('Tales By MoonLight', 'Njoli Patrick8', 'https://ibb.co/z4Q4L1N', 'this is a new book', 500.0, 'available');
-INSERT INTO books(title, author, image, description, price, status )VALUES ('Tales By MoonLight', 'Njoli Patrick9', 'https://ibb.co/z4Q4L1N', 'this is a new book', 500.0, 'available');
-INSERT INTO books(title, author, image, description, price, status )VALUES ('Tales By MoonLight', 'Njoli Patrick0', 'https://ibb.co/z4Q4L1N', 'this is a new book', 500.0, 'available');
-INSERT INTO books(title, author, image, description, price, status )VALUES ('Tales By MoonLight', 'Njoli1 Patrick', 'https://ibb.co/z4Q4L1N', 'this is a new book', 500.0, 'available');
-INSERT INTO books(title, author, image, description, price, status )VALUES ('Tales By MoonLight', 'Njoli2 Patrick', 'https://ibb.co/z4Q4L1N', 'this is a new book', 500.0, 'available');
-INSERT INTO books(title, author, image, description, price, status )VALUES ('Tales By MoonLight', 'Njoli3 Patrick', 'https://ibb.co/z4Q4L1N', 'this is a new book', 500.0, 'available');
-INSERT INTO books(title, author, image, description, price, status )VALUES ('Tales By MoonLight', 'Njoli4 Patrick', 'https://ibb.co/z4Q4L1N', 'this is a new book', 500.0, 'available');
-INSERT INTO books(title, author, image, description, price, status )VALUES ('Tales By MoonLight', 'Njoli5 Patrick', 'https://ibb.co/z4Q4L1N', 'this is a new book', 500.0, 'available');
-INSERT INTO books(title, author, image, description, price, status )VALUES ('Tales By MoonLight', 'Njoli6 Patrick', 'https://ibb.co/z4Q4L1N', 'this is a new book', 500.0, 'available');
-INSERT INTO books(title, author, image, description, price, status )VALUES ('Tales By MoonLight', 'Njoli7 Patrick', 'https://ibb.co/z4Q4L1N', 'this is a new book', 500.0, 'available');
-INSERT INTO books(title, author, image, description, price, status )VALUES ('Tales By MoonLight', 'Njoli8 Patrick', 'https://ibb.co/z4Q4L1N', 'this is a new book', 500.0, 'available');
-INSERT INTO books(title, author, image, description, price, status )VALUES ('Tales By MoonLight', 'Njoli9 Patrick', 'https://ibb.co/z4Q4L1N', 'this is a new book', 500.0, 'available');
-INSERT INTO books(title, author, image, description, price, status )VALUES ('Tales By MoonLight', 'Njoli0 Patrick', 'https://ibb.co/z4Q4L1N', 'this is a new book', 500.0, 'available');
-INSERT INTO books(title, author, image, description, price, status )VALUES ('Tales By MoonLight', 'Njoli 1Patrick', 'https://ibb.co/z4Q4L1N', 'this is a new book', 500.0, 'available');
-INSERT INTO books(title, author, image, description, price, status )VALUES ('Tales By MoonLight', 'Njoli 2Patrick', 'https://ibb.co/z4Q4L1N', 'this is a new book', 500.0, 'available');
-INSERT INTO books(title, author, image, description, price, status )VALUES ('Tales By MoonLight', 'Njoli 3Patrick', 'https://ibb.co/z4Q4L1N', 'this is a new book', 500.0, 'available');
-INSERT INTO books(title, author, image, description, price, status )VALUES ('Tales By MoonLight', 'Njoli 4Patrick', 'https://ibb.co/z4Q4L1N', 'this is a new book', 500.0, 'available');
-INSERT INTO books(title, author, image, description, price, status )VALUES ('Tales By MoonLight', 'Njoli 5Patrick', 'https://ibb.co/z4Q4L1N', 'this is a new book', 500.0, 'available');
-INSERT INTO books(title, author, image, description, price, status )VALUES ('Tales By MoonLight', 'Njoli 6Patrick', 'https://ibb.co/z4Q4L1N', 'this is a new book', 500.0, 'available');
-INSERT INTO books(title, author, image, description, price, status )VALUES ('Tales By MoonLight', 'Njoli 7Patrick', 'https://ibb.co/z4Q4L1N', 'this is a new book', 500.0, 'available');
-INSERT INTO books(title, author, image, description, price, status )VALUES ('Tales By MoonLight', 'Njoli 8Patrick', 'https://ibb.co/z4Q4L1N', 'this is a new book', 500.0, 'available');
-INSERT INTO books(title, author, image, description, price, status )VALUES ('Tales By MoonLight', 'Njoli 9Patrick', 'https://ibb.co/z4Q4L1N', 'this is a new book', 500.0, 'available');
-INSERT INTO books(title, author, image, description, price, status )VALUES ('Tales By MoonLight', 'Njoli 0Patrick', 'https://ibb.co/z4Q4L1N', 'this is a new book', 500.0, 'available');
-
+INSERT INTO books(title, author, image, description, price, status )VALUES ('Chike and the River', 'Chinua Achebe', 'https://folioreview.files.wordpress.com/2020/04/f21df8d465645236f369031b1dda8746.jpg', 'lorem jsdsh ikkkad hhjkjaa jkhjsks jkhsk js jusuhds ggtgywjhws n lorem djsjds loejh jklsdj kjsdjksdshn nf ndjh', 500.0, 'available');
+INSERT INTO books(title, author, image, description, price, status )VALUES ('Sugar Girl', 'Kola Onadipe', 'https://continentalbooksgh.com/wp-content/uploads/2020/05/Sugar-Girl.jpg','lorem jsdsh ikkkad hhjkjaa jkhjsks jkhsk js jusuhds ggtgywjhws n lorem djsjds loejh jklsdj kjsdjksdshn nf ndjh', 5000.0, 'available');
+INSERT INTO books(title, author, image, description, price, status )VALUES ('Without a Silver Spoon', 'Eddie Iroh', 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1347325638l/2301365.jpg','lorem jsdsh ikkkad hhjkjaa jkhjsks jkhsk js jusuhds ggtgywjhws n lorem djsjds loejh jklsdj kjsdjksdshn nf ndjh', 7000.0, 'available');
+INSERT INTO books(title, author, image, description, price, status )VALUES ('The African Child', 'Camera Laye', 'https://tothebalcony.files.wordpress.com/2012/08/africanchild1.jpg?w=584', 'lorem jsdsh ikkkad hhjkjaa jkhjsks jkhsk js jusuhds ggtgywjhws n lorem djsjds loejh jklsdj kjsdjksdshn nf ndjh', 5000.0, 'available');
+INSERT INTO books(title, author, image, description, price, status )VALUES ('The Concubine', 'Amadin Elechi', 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1281647970l/1199163.jpg', 'lorem jsdsh ikkkad hhjkjaa jkhjsks jkhsk js jusuhds ggtgywjhws n lorem djsjds loejh jklsdj kjsdjksdshn nf ndjh', 5000.0, 'available');
+INSERT INTO books(title, author, image, description, price, status )VALUES ('Half of a Yellow Sun', 'Chimamanda Adieche', 'https://farafinabooks.files.wordpress.com/2013/04/half-of-a-yellow-sun.jpg', 'lorem jsdsh ikkkad hhjkjaa jkhjsks jkhsk js jusuhds ggtgywjhws n lorem djsjds loejh jklsdj kjsdjksdshn nf ndjh', 1000.0, 'available');
+INSERT INTO books(title, author, image, description, price, status )VALUES ('There was a Country', 'Chinua Achebe', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYSZI4k6afyB1AUj8OR74jFcsaYr2E6NkHLg&usqp=CAU', 'lorem jsdsh ikkkad hhjkjaa jkhjsks jkhsk js jusuhds ggtgywjhws n lorem djsjds loejh jklsdj kjsdjksdshn nf ndjh', 8000.0, 'available');
--- src/v1/utiles/auth.utile.ts
+++ src/v1/utiles/auth.utile.ts
@@ -1,4 +1,4 @@
-import jwt, { JwtPayload } from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
 import { NextFunction, Request, Response } from 'express';
 import CustomError from './error.utile';
 import { config } from 'dotenv';
