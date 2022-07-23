cf3bfa49fff6600d6d8f6f0095553a15be4fb71a
Njoli Patrick <ogmaro@gmail.com>
2022-07-23 19:12:03 +0100

    added integration test for register endpoint

--- tests/v1/utiles/models/globalSpec.ts
+++ tests/v1/utiles/models/globalSpec.ts
@@ -1,47 +1,94 @@
-// import request from 'supertest';
-// import app from '../../../../src/app';
-// import { User } from '../../../../src/v1/models/auth.model';
-// import globalModel from '../../../../src/v1/models/global.model';
+import request from 'supertest';
+import app from '../../../../src/app';
+import { User } from '../../../../src/v1/models/auth.model';
+import globalModel from '../../../../src/v1/models/global.model';
 
-// describe('::::>Test Global Model', async () => {
-//     let originalTimeout: number;
 
-//     beforeAll(async () => {
-//         originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
-//         jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
-//     });
+describe('::::> Test User', () => {
+    let originalTimeout: number;
 
-//     afterAll(() => {
-//         jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
-//     });
+    beforeAll(() => {
+        originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+    });
 
-//     it('Check Model should return true if found and falsy if not found', async () => {
-//         const CheckUser = await globalModel.CHECKMODEL('USERS', 'id', 1);
-//         if (CheckUser) {
-//             expect(CheckUser).toBeTruthy();
-//         } else {
-//             expect(CheckUser).toBeFalsy();
-//         }
-//     });
+    afterAll(() => {
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+    });
+    it(' Request /api/v1/auth/register should return token and an instance of an Object', async () => {
+        const result = await request(app)
+            .post('/api/v1/auth/register')
+            .send({ firstname: 'JOB', lastname: 'ROW', username: 'thil@gmai', email: 'thil@gmai.com', password: 'admin123', password_confirmation: 'admin123' });
 
-//     it('Find All should return an array of available users', async () => {
-//         const findall: User[] = await globalModel.FINDALL('USERS', 10);
+        expect(result.body.success).toEqual(true);
+        expect(result.body.data).toBeInstanceOf(Object);
+        expect(result.body.data.token).toBeInstanceOf(String);
+        expect(result.status).toEqual(201);
+    });
+    it(' Request /api/v1/auth/register should return token and an instance of an Object', async () => {
+        const result = await request(app)
+            .post('/api/v1/auth/register')
+            .send({ firstname: 'JOB', lastname: 'ROW', username: 'thil@gmai', email: 'thil@gmai.com', password: 'admin123', password_confirmation: 'admin123' });
+        console.log(result.status, result.statusCode);
 
-//         expect(findall).toBeInstanceOf(Array);
-//     });
+        expect(result.body.success).toEqual(false);
+        expect(result.body.message).toBeInstanceOf(String);
+        expect(result.status).toEqual(400);
+    });
 
-//     it('Find One should return an Object of user', async () => {
-//         const findone: User = await globalModel.FINDONE('USERS', 'id', 10);
-//         expect(findone).toBeInstanceOf(Object);
-//     });
 
-//     it('Find Where should return an Array of Users', async () => {
-//         const findwhere: User[] = await globalModel.FINDWHERE('USERS', 'id', 1);
-//         expect(findwhere).toBeInstanceOf(Array);
-//     });
+    // it('Request /api/v1/user/ should return status 200 and an array', async () => {
+    //     const register = await request(app)
+    //         .post('/api/v1/user/register')
+    //         .send({ firstName: 'JOB', lastName: 'ROW', email: 'thij@gmai.com', password: 'password', role: 'admin' });
+    //     const token = register.body.response[0].token;
+    //     const result = await request(app)
+    //         .get('/api/v1/user/')
+    //         .send({ 'token': token });
+    //     expect(result.body.statusCode).toBe(200);
+    //     expect(result.body.response).toBeInstanceOf(Array);
+    // });
+    // it('Request /api/v1/user/ should return status 403 - unautheticated', async () => {
+    //     const result = await request(app)
+    //         .get('/api/v1/user/');
+    //     expect(result.status).toBe(403);
+    // });
+    // it('Request /api/v1/user/:id should return an array of a single user', async () => {
+    //     const register = await request(app)
+    //         .post('/api/v1/user/register')
+    //         .send({ firstName: 'JOB', lastName: 'ROW', email: 'thl@gmai.com', password: 'password', role: 'admin' });
+    //     const token = register.body.response[0].token;
+    //     const result = await request(app)
+    //         .get('/api/v1/user/1')
+    //         .send({ 'token': token });
+    //     expect(result.body.statusCode).toBe(200);
+    //     expect(result.body.response).toBeInstanceOf(Array);
 
-//     it('Find All should return an Array of Users', async () => {
-//         const findall: User[] = await globalModel.FINDALL('USERS', 10);
-//         expect(findall).toBeInstanceOf(Array);
-//     });
-// });
+    // });
+    // it('Request /api/v1/user/login should return status 200 and token', async () => {
+    //     const result = await request(app)
+    //         .post('/api/v1/user/login')
+    //         .send({ email: 'thil@gmai.com', password: 'password' });
+
+    //     expect(result.body.statusCode).toBe(200);
+    //     expect(result.body.response).toBeInstanceOf(Array);
+    //     expect(result.body.response[0].token).toBeInstanceOf(String);
+
+    // });
+    // it(' Request /api/v1/user/ should return  deleted user ', async () => {
+    //     const login = await request(app)
+    //         .post('/api/v1/user/login')
+    //         .send({ email: 'thil@gmai.com', password: 'password' });
+    //     const token = login.body.response[0].token;
+    //     const result = await request(app)
+    //         .get('/api/v1/user/1')
+    //         .send({ 'token': token });
+    //     expect(result.body.statusCode).toBe(200);
+    //     expect(result.body.response).toBeInstanceOf(Array);
+    // });
+    // it('Request * should return status 404', async () => {
+    //     const result = await request(app).get('/');
+    //     expect(result.status).toBe(404);
+    // });
+});
+
